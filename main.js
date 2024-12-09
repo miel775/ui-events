8aa2ea207e185838265e034fbe47eb8c6b8844c6
miel775 <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2024-12-09 12:42:22 +0100

    5 elementen

--- main.js
+++ main.js
@@ -25,11 +25,65 @@ interaction.addEventListener('animationend', jumpHandler)
 // Ga zelf verder met de overige elementen, aan de hand van de instructies
 // Maak bijvoorbeeld een bibber animatie als je op iets klikt
 
+let draaiorgel = document.querySelector('a:nth-of-type(1)')
 // Stap 1: querySelector
 // let bibberLink = document.querySelector...
 
 // Stap 2: addEventListener
 // bibberLink.addEventListener...
+draaiorgel.addEventListener('dblclick', draaien)
+// hier heb ik dubbelklik gebruikt
 
 // Stap 3: (Callback functie met) classList (.toggle(), .add(), etc.)
 // bibberLink.classList.toggle...
+function draaien() {
+
+  draaiorgel.classList.toggle('turn')
+}
+
+// tweede
+let bibberen = document.querySelector('a:nth-of-type(2)')
+
+bibberen.addEventListener('click', bibs)
+
+function bibs() {
+  bibberen.classList.toggle('bibberen')
+}
+
+// derde
+let plat = document.querySelector('a:nth-of-type(3)')
+
+plat.addEventListener('click', platter)
+
+function platter() {
+  plat.classList.toggle('platmaken')
+}
+
+// vierde
+let regenboog = document.querySelector('a:nth-of-type(4)')
+
+regenboog.addEventListener('click', regen)
+
+function regen() {
+  regenboog.classList.toggle('regenboog')
+  console.log(regenboog)
+}
+
+// vijfde
+
+let toets = document.querySelector('a:nth-of-type(5)')
+
+document.addEventListener('keydown', lol, true)
+
+// hier een keyfunctie uitgevoerd
+function lol(e) {
+  
+	if (e.key ===  '4') {
+    regenboog.classList.toggle('regenboog')
+		
+	} else if (e.key === '5') {
+    plat.classList.toggle('platmaken')
+  }
+}
+
+
